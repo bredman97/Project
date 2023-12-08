e20d37103e208524c40879d4c7a8919eeba525f0
HitanshuDobariya <146300967+HitanshuDobariya@users.noreply.github.com>
2023-12-08 00:03:46 +0000

    manga databse page

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './partials/header/header.component'; 
 import { FooterComponent } from './partials/footer/footer.component';
-
+import { MangaComponent } from './pages/manga/manga.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,FooterComponent, MangaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
